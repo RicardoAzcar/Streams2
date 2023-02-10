89d9ed663cf99665b144afdb30d2f7e03cd19abe
Isaul Mendieta Galindo <imendieta@minsait.com>
2023-02-10 13:11:59 -0600

    Ejercicios Resueltos

---
+++ src/main/java/MGI/Coleccion.java
@@ -0,0 +1,78 @@
+package MGI;
+
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Comparator;
+
+import java.util.stream.Collectors;
+
+
+// Y la siguiente coleccion
+public class Coleccion {
+    public static void main(String[] args) {
+        List<Curso> cursos = new ArrayList<>();
+        cursos.add(new Curso("Cursos profesional de Java", 6.5f, 50, 200));
+        cursos.add(new Curso("Cursos profesional de Python", 8.5f, 60, 800));
+        cursos.add(new Curso("Cursos profesional de DB", 4.5f, 70, 700));
+        cursos.add(new Curso("Cursos profesional de Android", 7.5f, 10, 400));
+        cursos.add(new Curso("Cursos profesional de Escritura", 1.5f, 10, 300));
+
+        // Obtener la cantidad de cursos con una duración mayor a 5 horas.
+        long cantMayorCinco = cursos.stream().filter(curso -> curso.getDuracion() > 5).count();
+
+        //Obtener la cantidad de cursos con una duración menor a 2 horas.
+        long cantMenorDos = cursos.stream().filter(curso -> curso.getDuracion() < 2).count();
+
+        //Listar el título de todos aquellos cursos con una cantidad de vídeos mayor a 50.
+        List<Curso> list = cursos.stream().filter(curso -> curso.getVideos() > 50).toList();
+
+        // Mostrar en consola el título de los 3 cursos con mayor duración.
+        cursos.stream()
+                .sorted(Comparator.comparing(Curso::getDuracion))
+                .collect(Collectors.toList())
+                .subList(cursos.size() - 3, cursos.size()).forEach(curso -> System.out.println(curso.getTitulo()));
+
+        //Mostrar en consola la duración total de todos los cursos.
+        System.out.println("Duracion de todos los cursos " + cursos.stream().mapToDouble(Curso::getDuracion).sum());
+
+
+        // Mostrar en consola todos aquellos cursos que superen el promedio en cuanto a duración se refiere.
+        cursos.stream()
+                .filter(curso -> curso.getDuracion() >  cursos.stream()
+                        .mapToDouble(Curso::getDuracion)
+                        .average()
+                        .getAsDouble())
+                .forEach(System.out::println);
+
+        // Mostrar en consola la duración de todos aquellos cursos que tengan una
+        // cantidad de alumnos inscritos menor a 500.
+        cursos.stream().filter(curso -> curso.getAlumnos()<500).forEach(curso -> System.out.println(curso.getDuracion()));
+
+        // Obtener el curso con mayor duración.
+        Curso cursoMayorDuracion = cursos.stream()
+                .max(Comparator.comparingDouble(Curso::getDuracion))
+                .orElse(null);
+
+        //Crear una lista de Strings con todos los titulos de los cursos.
+        List<String> listaTitulos = cursos.stream().map(Curso::getTitulo).collect(Collectors.toList());
+     listaTitulos.forEach(System.out::println);
+    }
+
+
+    /***
+     * Resolver los siguientes problemas con streams:
+     * Obtener la cantidad de cursos con una duración mayor a 5 horas.
+     * Obtener la cantidad de cursos con una duración menor a 2 horas.
+     * Listar el título de todos aquellos cursos con una cantidad de vídeos mayor a 50.
+     * Mostrar en consola el título de los 3 cursos con mayor duración.
+     * Mostrar en consola la duración total de todos los cursos.
+     * Mostrar en consola todos aquellos cursos que superen el promedio en cuanto a duración se refiere.
+     * Mostrar en consola la duración de todos aquellos cursos que tengan una
+     * cantidad de alumnos inscritos menor a 500.
+     * Obtener el curso con mayor duración.
+     * Crear una lista de Strings con todos los titulos de los cursos.
+     *
+     * Una vez terminado subirlo al repositorio
+     */
+}
